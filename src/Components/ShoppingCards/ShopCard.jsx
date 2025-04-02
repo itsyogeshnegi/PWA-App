a9cf5424601897d6de8b64ce072ee2ee56294919
Yogesh <yogesh.negi@wemonde.com>
2025-04-02 15:26:56 +0530

    algin set

--- src/Components/ShoppingCards/ShopCard.jsx
+++ src/Components/ShoppingCards/ShopCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const ShopCard = ({data}) => {
   return (
-    <div className='h-auto w-64 border-2 border-black p-1'>
+    <div className='h-auto w-64 border-2 border-black p-1 rounded-lg'>
         <div>
-            <img src={data?.image}/>
+            <img className='rounded-lg' src={data?.image}/>
+        </div>
             <p>{data?.name}</p>
             <p>Rating : {data?.rating}/5</p>
             <p>Totoal Review : {data?.reviewCount}</p>
-        </div>
     </div>
   )
 }
